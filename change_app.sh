71f4dd88ad347600bbcf3fc0a23ff0c5e9d0d33c
liqingju123 <973683374@qq.com>
2020-04-12 11:18:52 +0800

    去除弹框

--- change_app.sh
+++ change_app.sh
@@ -2,14 +2,19 @@
 
 while :
 do
-	monkey -p com.ss.android.ugc.livelite 1 ; #启动火山小视频
-	sleep 300;
-	monkey -p com.ss.android.ugc.aweme.lite 1 ; #抖音极速版
-	sleep  300;
-	monkey -p  com.kuaishou.nebula 1; # 快手启动
-	sleep 300;
+	#monkey -p com.ss.android.ugc.livelite 1 ; #启动火山小视频
+	#sleep 300;
+	#monkey -p com.ss.android.ugc.aweme.lite 1 ; #抖音极速版
+	#sleep  300;
+	#monkey -p  com.kuaishou.nebula 1; # 快手启动
+#	sleep 300;
 	monkey -p com.jm.video 1; # 刷宝短视频
 	sleep  300;
 	monkey -p com.video.kd 1; # 快视频
 	sleep 300;
+	monkey -p  com.video.gs 1; #高手
+	sleep 300;
+	monkey -p   com.video.yy 1; #有颜
+	sleep 300;
+	monkey -p   com.video.xch 1; # 小吃货
 done;
---
+++ input_keyevent.sh
@@ -0,0 +1,7 @@
+while :
+do
+
+	input keyevent 4;
+	let s1=$(($(($RANDOM%5))+2));
+	sleep $s1 ;
+done;
